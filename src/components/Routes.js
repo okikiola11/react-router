a514a6f34391e77119d4aab7a53ddcd8bb87d95c
okikiola11 <okikiola.apelehin@gmail.com>
2021-06-07 15:31:50 +0000

    Add a new settings route to the Route.js file Add Route component to the index.js file so as to load the Router component first

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
 import Profile from './Profile';
+import Settings from './Settings';
 
 const Routes = () => {
   return (
@@ -9,6 +10,7 @@ const Routes = () => {
       <Switch>
         <Route exact path='/' component={App} />
         <Route exact path='/profile' component={Profile} />
+        <Route exact path='/settings' component={Settings} />
       </Switch>
     </BrowserRouter>
   )
---
+++ src/components/Settings.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Settings = () => {
+  return (
+    <div>
+      <h1>New Settings and Protocol</h1>
+    </div>
+  )
+}
+
+export default Settings
